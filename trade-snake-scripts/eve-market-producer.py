7bfc78863df8355dd70fce0a3edf0c996645f2d8
Robert Stolz <rstolz@ucdavis.edu>
2020-10-11 12:29:59 -0700

    Improved the market producer script. Began producer/consumer -> pulsar function migration.

--- trade-snake-scripts/eve-market-producer.py
+++ trade-snake-scripts/eve-market-producer.py
@@ -3,31 +3,36 @@ import pulsar
 import time
 from pulsar.schema import *
 import json
+import time
 
-SLEEP_TIME = 1
+SLEEP_TIME = 0
 broker_url = "pulsar://ec2-13-52-145-55.us-west-1.compute.amazonaws.com:6650"
 
 client = pulsar.Client(broker_url)
-#producer = client.create_producer(topic='raw-orders',producer_name='market-producer',schema=StringSchema())
+producer = client.create_producer(topic='raw-orders', producer_name='market-producer', schema=StringSchema())
+market_stats_producer = client.create_producer(topic='market-stats', producer_name='market-stats-producer', schema=StringSchema())
 
 region_ids = requests.get("https://esi.evetech.net/latest/universe/regions/?datasource=tranquility")
 region_ids = region_ids.text[1:-1].split(',')
-region_ids = [x for x in map(int,region_ids) if x < 11000000] #filter out wh region ids
+region_ids = [x for x in map(int,region_ids) if x < 11000000]
 
-market_pull_index = 0
 while(True):
+    current_market_stats = {'market_pull_index': 0, 'n_orders': 0, 'pull_time': time.time()}
     payload = {'datasource':'tranquility','ordertype':'all','page':'1'}
-    for i in region_ids:
-        j = 1
+    for region in region_ids:
+        page = 1
         n_pages = 2
-        while j < n_pages + 1:
-            payload['page'] = str(j)
-            r = requests.get("https://esi.evetech.net/latest/markets/" + repr(i) + "/orders/", payload)
+        while page < n_pages + 1:
+            payload['page'] = str(page)
+            r = requests.get("https://esi.evetech.net/latest/markets/" + repr(region) + "/orders/", payload)
             n_pages = int(r.headers['x-pages'])
             for k in r.json():
-                print(json.dumps(k))
-                #producer.send(json.dumps(k))
-            j += 1
-    market_pull_index += 1
+                #print(json.dumps(k))
+                producer.send(json.dumps(k))
+                current_market_stats['n_orders'] += 1
+            page += 1
+    current_market_stats['market_pull_index'] += 1
+    current_market_stats['pull_time'] = time.time() - current_market_stats['pull_time']
+    market_stats_producer.send(json.dumps(current_market_stats))
     time.sleep(SLEEP_TIME)
 
---
+++ trade-snake-scripts/pf-sort-raw-orders.py
@@ -0,0 +1,9 @@
+from pulsar.functions.function import Function
+import json
+import redis
+
+class Pf_sort_raw_orders(Function):
+    def __init__(self):
+        pass
+    def process(self, input, context):
+        pass
